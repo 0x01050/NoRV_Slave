a9eaeb9acd7787d7b68db04e2ca46317848c6551
0x01050 <jeraleking@gmail.com>
2020-06-22 21:57:59 +0300

    v 1.0.0

---
+++ Config.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Slave
+{
+    class Config
+    {
+        private static Config _instance = null;
+        public static Config getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Config();
+            }
+            return _instance;
+        }
+
+
+        Config()
+        {
+            LoadGlobalConfig();
+        }
+
+        // Global Config
+        private string _serverUrl = "";
+        private void LoadGlobalConfig()
+        {
+            var xml = XDocument.Load(@"Config.xml");
+            var query = from c in xml.Root.Descendants("Item")
+                        select c;
+            foreach (var item in query)
+            {
+                switch ((string)item.Attribute("Key"))
+                {
+                    case "ServerUrl":
+                        _serverUrl = (string)item.Attribute("Value");
+                        break;
+                }
+            }
+        }
+        public string getServerUrl()
+        {
+            return _serverUrl;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Slave
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            RegisterScreen prompt = new RegisterScreen();
+            Application.Run(prompt);
+            if (prompt.DialogResult != DialogResult.OK)
+                return;
+
+            Thread thread = new Thread(new ThreadStart(reportStatus));
+            thread.Start();
+            Application.Run();
+            thread.Abort();
+        }
+
+        private static void reportStatus()
+        {
+            while(true)
+            {
+                try
+                {
+                    var httpClient = new HttpClient();
+                    string url = Config.getInstance().getServerUrl() + "/status/slave";
+                    MultipartFormDataContent httpContent = new MultipartFormDataContent();
+                    httpContent.Add(new StringContent(L.getID()), "id");
+                    httpClient.PostAsync(url, httpContent);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+                Thread.Sleep(30 * 1000);
+            }    
+        }
+    }
+}
